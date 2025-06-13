1113673c79adea251af33c368d8ee15ed4088b4d
daviddluci <gliaubertasdeividas@gmail.com>
2025-06-13 14:29:23 +0300

    add collection of feedbacks to event

--- src/main/java/com/example/eventsync/api/model/Event.java
+++ src/main/java/com/example/eventsync/api/model/Event.java
@@ -1,5 +1,9 @@
 package com.example.eventsync.api.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,12 +16,17 @@ public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String title;
     private String description;
+
+    @ElementCollection
+    private List<Feedback> feedbacks;
 
     public Event(String title, String description){
         this.title = title;
         this.description = description;
+        feedbacks = new ArrayList<>();
     }
 
     protected Event(){}
@@ -44,5 +53,13 @@ public class Event {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public void addFeedback(String text){
+        feedbacks.add(new Feedback(text));
+    }
+
+    public List<Feedback> getFeedbacksCopy(){
+        return List.copyOf(feedbacks);
     }
 }
