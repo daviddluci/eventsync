e49b224846c691a3b167bfe013d73aedffa96cbf
daviddluci <gliaubertasdeividas@gmail.com>
2025-06-13 14:30:30 +0300

    add sentiment analysis and feedback submission

--- src/main/java/com/example/eventsync/service/EventService.java
+++ src/main/java/com/example/eventsync/service/EventService.java
@@ -3,8 +3,13 @@ package com.example.eventsync.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import com.example.eventsync.api.dto.SentimentResponse;
+import com.example.eventsync.api.dto.SentimentScore;
 import com.example.eventsync.api.model.Event;
 import com.example.eventsync.repository.EventRepository;
 
@@ -13,6 +18,9 @@ import jakarta.annotation.PostConstruct;
 @Service
 public class EventService {
     private final EventRepository eventRepository;
+
+    @Value("${huggingface.api.key}")
+    private String huggingfaceApiKey;
 
     public EventService(EventRepository eventRepository){
         this.eventRepository = eventRepository;
@@ -36,8 +44,44 @@ public class EventService {
     public List<Event> getAllEvents(){
         return eventRepository.findAll();
     }
-
+    
     public void addEvent(Event event){
         eventRepository.save(event);
+    }
+
+    public void submitFeedback(Long id, String text){
+        Event event = eventRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Event not found with id: " + id));
+        
+        event.addFeedback(text);
+        eventRepository.save(event);
+        SentimentResponse sentiment = analyzeSentiment(text);
+        for (List<SentimentScore> l : sentiment.getScores()){
+            for (SentimentScore s : l){
+                System.out.println(s.getLabel() + " " + s.getScore());
+            }
+        }
+    }
+
+    private SentimentResponse analyzeSentiment(String text) {
+        String url = "https://router.huggingface.co/hf-inference/models/cardiffnlp/twitter-roberta-base-sentiment";
+
+        WebClient.Builder builder = WebClient.builder();
+
+        String requestBody = "{ \"inputs\": \"" + text.replace("\"", "\\\"") + "\" }";
+
+        List<List<SentimentScore>> sentimentScores = builder.build()
+            .post()
+            .uri(url)
+            .header("Authorization", "Bearer " + huggingfaceApiKey)
+            .header("Content-Type", "application/json")
+            .bodyValue(requestBody)
+            .retrieve()
+            .bodyToMono(new ParameterizedTypeReference<List<List<SentimentScore>>>() {})
+            .block();
+
+        SentimentResponse sentiment = new SentimentResponse(sentimentScores);
+
+        return sentiment;
     }
 }
