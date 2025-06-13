b35e72684b7a343bfba88d0e15d61990370574d7
daviddluci <gliaubertasdeividas@gmail.com>
2025-06-13 14:23:45 +0300

    feat: add postmapping for submitting feedback

--- src/main/java/com/example/eventsync/api/controller/EventController.java
+++ src/main/java/com/example/eventsync/api/controller/EventController.java
@@ -6,12 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.eventsync.api.model.Event;
-import com.example.eventsync.service.EventService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.eventsync.api.dto.FeedbackRequest;
+import com.example.eventsync.api.model.Event;
+import com.example.eventsync.service.EventService;
 
 
 
@@ -24,23 +25,31 @@ public class EventController {
         this.eventService = eventService;
     }
 
+    @PostMapping("/events")
+    public Event addEvent(@RequestBody Event event) {        
+        eventService.addEvent(event);
+        return event;
+    }
+
     @GetMapping("/events")
     public List<Event> getEvents() {
         return eventService.getAllEvents();
     }
+
+    @PostMapping("/events/{id}/feedback")
+    public ResponseEntity<Event> submitFeedback(@PathVariable Long id,@RequestBody FeedbackRequest feedbackRequest) {
+        eventService.submitFeedback(id, feedbackRequest.getText());
+        return eventService.getEvent(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+    
 
     @GetMapping("/events/{id}/summary")
     public ResponseEntity<Event> getEvent(@PathVariable Long id) {
         return eventService.getEvent(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
-    }
-
-
-    @PostMapping("/events")
-    public Event addEvent(@RequestBody Event event) {        
-        eventService.addEvent(event);
-        return event;
     }
     
     
