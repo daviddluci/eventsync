3ec0fa22ed6ea0c749b74c327a5fe7b0120accd8
daviddluci <gliaubertasdeividas@gmail.com>
2025-06-14 13:58:52 +0300

    create sentiment stats to track quantity of different feedback sentiment

---
+++ src/main/java/com/example/eventsync/api/model/SentimentStats.java
@@ -0,0 +1,66 @@
+package com.example.eventsync.api.model;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class SentimentStats {
+    private Long positiveCount;
+    private Long neutralCount; 
+    private Long negativeCount; 
+    private Long unrecognizedCount;
+
+    public SentimentStats () {
+        positiveCount = 0L;
+        neutralCount = 0L;
+        negativeCount = 0L;
+        unrecognizedCount = 0L;
+    }
+
+    public void incrementPositive() {
+        this.positiveCount++;
+    }      
+
+    public void incrementNeutral() {
+        this.neutralCount++;
+    }
+
+    public void incrementNegative() {
+        this.negativeCount++;
+    }
+
+    public void incrementUnrecognized(){
+        this.unrecognizedCount++;
+    }
+    
+    public Long getPositiveCount() {
+        return positiveCount;
+    }
+
+    public void setPositiveCount(Long positiveCount) {
+        this.positiveCount = positiveCount;
+    }
+
+    public Long getNeutralCount() {
+        return neutralCount;
+    }
+
+    public void setNeutralCount(Long neutralCount) {
+        this.neutralCount = neutralCount;
+    }
+
+    public Long getNegativeCount() {
+        return negativeCount;
+    }
+
+    public void setNegativeCount(Long negativeCount) {
+        this.negativeCount = negativeCount;
+    }
+
+    public Long getUnrecognizedCount() {
+        return unrecognizedCount;
+    }
+
+    public void setUnrecognizedCount(Long unrecognizedCount) {
+        this.unrecognizedCount = unrecognizedCount;
+    }
+}
